6512e82daf5b5f96b0decfd8914a63c4544ae195
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-04-18 22:22:52 +0100

    import path in vite config

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,13 +1,13 @@
-const path = require('path')
-import { loadEnv, defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react';
-import { urbitPlugin } from '@urbit/vite-plugin-urbit';
+import path from 'path'
+import { loadEnv, defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react'
+import { urbitPlugin } from '@urbit/vite-plugin-urbit'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  Object.assign(process.env, loadEnv(mode, process.cwd()));
-  const SHIP_URL = process.env.SHIP_URL || process.env.VITE_SHIP_URL || 'http://localhost:8080';
-  console.log(SHIP_URL);
+  Object.assign(process.env, loadEnv(mode, process.cwd()))
+  const SHIP_URL = process.env.SHIP_URL || process.env.VITE_SHIP_URL || 'http://localhost:8080'
+  console.log(SHIP_URL)
 
   return defineConfig({
     build: {
@@ -16,6 +16,6 @@ export default ({ mode }) => {
     plugins: [
       urbitPlugin({ base: 'chess', target: SHIP_URL, secure: false }),
       reactRefresh()
-    ],
-  });
-};
+    ]
+  })
+}
